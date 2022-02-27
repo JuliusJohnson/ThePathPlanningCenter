92ea59d59c16c39e3e6a81b9d4b791e4dc8ed8e5
Julius Johnson <contact@juliusj.com>
2022-02-26 20:52:55 -0800

    finished functions for planitems portion

--- planitems.py
+++ planitems.py
@@ -1,3 +1,4 @@
+from asyncore import write
 import json
 from urllib.request import Request
 import requests
@@ -5,6 +6,7 @@ import sqlite3
 import sendRequest
 from pprint import pprint
 from config import credentials
+from datetime import date
 
 conn = sqlite3.connect('/home/julius/Documents/python/projects/thepath/PlanningCenter')
 connectionCursor = conn.cursor()
@@ -14,6 +16,12 @@ def getSelfLinkFromDB():
         nextPlanLink = row[0]
         return str(nextPlanLink) + "/items"
 
+def writeRequestToDB(data):
+    conn.execute("insert into PlanningCenterItems values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+    (data['attributes']['created_at'], data['attributes']['custom_arrangement_sequence'], data['attributes']['custom_arrangement_sequence_full'], data['attributes']['custom_arrangement_sequence_short'], data['attributes']['description'],data['attributes']['html_details'],data['attributes']['item_type'],data['attributes']['key_name'],data['attributes']['length'], data['attributes']['sequence'], data['attributes']['service_position'], data['attributes']['title'], data['attributes']['updated_at'], data['links']['self'], date.today(), data['relationships']['plan']['data']['id']))
+    conn.commit()
+    pprint("Update Successful")
 
 x = getSelfLinkFromDB()
-pprint(sendRequest.sendRequest(x))
+y = (sendRequest.sendRequest(x)[0])
+writeRequestToDB(y)
